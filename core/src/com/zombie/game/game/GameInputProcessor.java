ccd31bf8a3d22a2ff47f5a2a0836c9aaa61fec87
Sergey Rubtsov <dart.moal@gmail.com>
2016-02-11 01:10:28 +0300

    some control changes

--- core/src/com/zombie/game/game/GameInputProcessor.java
+++ core/src/com/zombie/game/game/GameInputProcessor.java
@@ -6,13 +6,14 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Align;
 import com.zombie.game.actors.Pointer;
-import com.zombie.game.steering.SteeringActor;
 
 public class GameInputProcessor implements InputProcessor {
 
-    private int touchPressed = -1;
+    private int mouseButton = -1;
 
     final OrthographicCamera camera;
+    final Vector2 scenePosition = new Vector2();
+
     final Vector3 curr = new Vector3();
     final Vector3 last = new Vector3(-1, -1, -1);
     final Vector3 delta = new Vector3();
@@ -30,7 +31,6 @@ public class GameInputProcessor implements InputProcessor {
     @Override
     public boolean keyDown(int keycode) {
         System.out.println("keyDown:" + keycode);
-
         camera.update();
         return false;
     }
@@ -48,71 +48,52 @@ public class GameInputProcessor implements InputProcessor {
     }
 
     @Override
-    public boolean touchDown(int x, int y, int pointer, int button) {
-        System.out.println("touchDown x:" + x + " y:" + y + " pointer:" + pointer + " button:" + button);
-        //camera.position.x = x;
-        //camera.position.y = y;
-/*        camera.unproject(curr.set(x, y, 0));
-        if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
-            camera.unproject(delta.set(last.x, last.y, 0));
-            delta.sub(curr);
-            camera.position.add(delta.x, delta.y, 0);
-        }
-        last.set(x, y, 0);*/
-        setTargetPosition(x, y);
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        this.mouseButton = button;
         return true;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        System.out.println("touchUp x:"+ screenX + " y:" + screenY + " pointer:" + pointer + " button:" + button);
+        this.mouseButton = -1;
         last.set(-1, -1, -1);
         return false;
     }
 
     @Override
-    public boolean touchDragged(int x, int y, int pointer) {
-        camera.unproject(curr.set(x, y, 0));
-        if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
-            camera.unproject(delta.set(last.x, last.y, 0));
-            delta.sub(curr);
-            camera.position.add(delta.x, delta.y, 0);
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        setCurrentPosition(screenX, screenY);
+        if (mouseButton > 0) {
+            camera.unproject(curr.set(screenX, screenY, 0));
+            if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
+                camera.unproject(delta.set(last.x, last.y, 0));
+                delta.sub(curr);
+                camera.position.add(delta.x, delta.y, 0);
+            }
+            last.set(screenX, screenY, 0);
+        } else if (mouseButton == 0) {
+
         }
-        last.set(x, y, 0);
-        Vector3 res = camera.unproject(new Vector3(x, y, 0));
-        System.out.println("camera ux:" + res.x + " camera x:" + camera.position.x + " camera uy:" + res.y
-                + " camera y:" + camera.position.y + " camera uz:" + res.z  + " camera z:" + camera.position.z);
-        System.out.println(  );
         return true;
     }
 
     public void setCurrentPosition(int screenX, int screenY) {
-
+        Vector3 c = camera.unproject(curr.set(screenX, screenY, 0));
+        scenePosition.x = c.x;
+        scenePosition.y = c.y;
     }
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
-        Vector2 pos = new Vector2(screenX, screenY);
-        pos = scene.getStage().screenToStageCoordinates(pos);
-        System.out.println("mouseMoved screenX:" + screenX + " screenY:" + screenY + " x:" + pos.x + " y:" + pos.y);
+        setCurrentPosition(screenX, screenY);
         return false;
     }
 
     @Override
     public boolean scrolled(int amount) {
-        camera.zoom = camera.zoom + amount;
         if (camera.zoom + amount >= 1){
-            if (amount > 0) {
-
-            } else {
-
-            }
             camera.zoom = camera.zoom + amount;
         } else camera.zoom = 1;
-
-        camera.update();
-        System.out.println("scrolled amount:" + amount + " camera.zoom:" + camera.zoom
-                + " camera.position x:" + camera.position.x + " camera.position y:" + camera.position.y);
         return false;
     }
 
