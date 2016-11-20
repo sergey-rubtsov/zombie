c4c62f85ca60fec63a4a5f3540c44fd8c1a79439
Sergey Rubtsov <dart.moal@gmail.com>
2016-11-20 15:43:49 +0200

    added unit test and yaml library

---
+++ desktop/test/com/zombie/game/game/TiledBackgroundTest.java
@@ -0,0 +1,47 @@
+package com.zombie.game.game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Serg on 19.11.2016.
+ */
+
+public class TiledBackgroundTest {
+
+    @Test
+    public void getTileWidth() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getTileHeight() throws Exception {
+
+    }
+
+    @Test
+    public void getWidth() throws Exception {
+
+    }
+
+    @Test
+    public void getHeight() throws Exception {
+
+    }
+
+    @Test
+    public void getMapWidth() throws Exception {
+
+    }
+
+    @Test
+    public void getMapHeight() throws Exception {
+
+    }
+
+    @Test
+    public void getCell() throws Exception {
+
+    }
+}
